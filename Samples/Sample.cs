a34e365bf95e88f90b0c67d2e59cbcc4ea15e5a8
sfcbetiuc <42634794+sfcbetiuc@users.noreply.github.com>
2019-11-05 12:21:47 +0200

    Added samples project (#8)
    * Added samples project
    
    * Modified namespaces

---
+++ Samples/Sample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Salesforce.MarketingCloud.Samples
+{
+    partial class Sample
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Sample to run:");
+                RunSample(Console.ReadLine());
+            }
+            else
+            {
+                foreach (string sampleName in args)
+                {
+                    RunSample(sampleName);
+                }
+            }
+            Console.WriteLine("Press Enter to Exit");
+            Console.ReadLine();
+        }
+
+        static void RunSample(string sampleName)
+        {
+            switch (sampleName)
+            {
+                case "SendEmailToMultipleRecipients":
+                    SendEmailToMultipleRecipients();
+                    break;
+                case "SendEmailToSingleRecipient":
+                    SendEmailToSingleRecipient();
+                    break;
+                default:
+                    Console.WriteLine("Unrecognized sample: " + sampleName);
+                    break;
+            }
+        }
+    }
+}
---
+++ Samples/SendEmailToMultipleRecipients.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Salesforce.MarketingCloud.Model;
+
+namespace Salesforce.MarketingCloud.Samples
+{
+    partial class Sample
+    {
+        static void SendEmailToMultipleRecipients()
+        {
+            // Replace '<CONTACT1 KEY>' and '<CONTACT2 KEY>' with real subscriber keys
+            const string contact1Key = "<CONTACT1 KEY>";
+            const string contact2Key = "<CONTACT2 KEY>";
+
+            // Environment variables instantiated client:
+            var client = new Api.Client();
+
+            // Get the asset, transactional messaging API instances:
+            var assetApi = client.AssetApi;
+            var transactionalMessagingApi = client.TransactionalMessagingApi;
+
+            // Create email send definition:
+            var emailDefinitionObject = SampleHelper.CreateEmailDefinitionObject(assetApi);
+            var createEmailDefinitionResult = transactionalMessagingApi.CreateEmailDefinition(emailDefinitionObject);
+
+            // Send email to multiple recipients:
+            var recipient1MessageKey = $"{Guid.NewGuid()}";
+            var recipient2MessageKey = $"{Guid.NewGuid()}";
+
+            var recipient1 = new Recipient(contact1Key, messageKey: recipient1MessageKey);
+            var recipient2 = new Recipient(contact2Key, messageKey: recipient2MessageKey);
+
+            var recipientsList = new List<Recipient>() {recipient1, recipient2};
+
+            var batchMessageRequestBody = new SendEmailToMultipleRecipientsRequest(createEmailDefinitionResult.DefinitionKey, recipientsList);
+
+            transactionalMessagingApi.SendEmailToMultipleRecipients(batchMessageRequestBody);
+
+            // Get the send status of the two email sends:
+            var recipient1SendStatus = transactionalMessagingApi.GetEmailSendStatusForRecipient(recipient1MessageKey);
+            var recipient2SendStatus = transactionalMessagingApi.GetEmailSendStatusForRecipient(recipient2MessageKey);
+        }
+    }
+}
